52a7e59e920e8f8c2c5e5f7b42fe670f919b1699
Malenia <82274616+TheMalenia@users.noreply.github.com>
2024-06-28 13:25:37 -0700

    Update Detective.java

--- Detective.java
+++ Detective.java
@@ -25,10 +25,18 @@ class ToDos {
     poirotsToDos.add("reveal the truth of the crime");
     
     // Print the size of each ArrayList below:
-
+    System.out.println("Sherlock: " + sherlocksToDos.size());
+    System.out.println("Poirot: " + poirotsToDos.size());
     
       
     // Print the name of the detective with the larger to-do list:
+   if (sherlocksToDos.size() > poirotsToDos.size()) {
+            System.out.println("Sherlock.");
+    } else if (sherlocksToDos.size() < poirotsToDos.size()) {
+        System.out.println("Poirot.");
+    } else {
+        System.out.println("Equal.");
+    }
     
   }
   
