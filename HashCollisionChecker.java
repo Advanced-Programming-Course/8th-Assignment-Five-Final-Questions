e8d5a13580e23fddfc363f755a3e57607550cc4d
rezaahmadisbu <Ahmadireza582@gmail.com>
2024-06-28 15:15:17 +0330

    done

--- HashCollisionChecker.java
+++ HashCollisionChecker.java
@@ -2,13 +2,19 @@ import java.util.*;
 
 public class HashCollisionChecker {
     public static <T> int countOfUniqueHashCodes(HashSet<T> set) {
-        // TODO: Implement
-        return 0;
+        HashSet<Integer> uniqueHashCodes = new HashSet<>();
+        for (T element : set) {
+            uniqueHashCodes.add(element.hashCode());
+        }
+        return uniqueHashCodes.size();
     }
 
     public static <K, V> int countOfUniqueHashCodes(HashMap<K, V> map) {
-        // TODO: Implement
-        return 0;
+        HashSet<Integer> uniqueHashCodes = new HashSet<>();
+        for (K key : map.keySet()) {
+            uniqueHashCodes.add(key.hashCode());
+        }
+        return uniqueHashCodes.size();
     }
 
     public static void main(String[] args) {
