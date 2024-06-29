8732fb3a3ffdb25a7b3afbeaa514ac573de01a11
MahsaMv <156520622+MahsaMv@users.noreply.github.com>
2024-06-29 11:21:12 +0330

    Update HashCollisionChecker.java

--- HashCollisionChecker.java
+++ HashCollisionChecker.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 public class HashCollisionChecker {
     public static <T> int countOfUniqueHashCodes(HashSet<T> set) {
-        // TODO: Implement
-        return 0;
+        HashSet<Integer> uniqueHashCodes = new HashSet<>();
+        for (T item : set) {
+            uniqueHashCodes.add(item.hashCode());
+        }
+        return uniqueHashCodes.size();
     }
-
     public static <K, V> int countOfUniqueHashCodes(HashMap<K, V> map) {
-        // TODO: Implement
-        return 0;
+        HashSet<Integer> uniqueHashCodes = new HashSet<>();
+        for (K key : map.keySet()) {
+            uniqueHashCodes.add(key.hashCode());
+        }
+        return uniqueHashCodes.size();
     }
+}
 
     public static void main(String[] args) {
         HashSet<String> set = new HashSet<>();
@@ -18,12 +24,12 @@ public class HashCollisionChecker {
         set.add("c#c#c#c#c#c#bBc#c#c#c#c#aa");
         set.add("1234");
         set.add("c#c#c#c#c#c#bBc#c#c#c#c#bB");
-        System.out.println(countOfUniqueHashCodes(set)); // 3
+        System.out.println(HashCollisionChecker.countOfUniqueHashCodes(set)); // 3
 
         HashMap<String, Integer> map = new HashMap<>();
         map.put("c#c#c#c#c#c#c#aaaaaaaabBbB", 14);
         map.put("c#c#c#c#c#c#c#aaaaaaaac#c#", 12);
         map.put("c#c#c#c#c#c#c#aaaaaaaac#cc", 16);
-        System.out.println(countOfUniqueHashCodes(map)); // 2
+        System.out.println(HashCollisionChecker.countOfUniqueHashCodes(map)); // 2
     }
 }
